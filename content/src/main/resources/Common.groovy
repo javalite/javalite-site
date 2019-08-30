39bc939255c8281f4ccd2f225daf4dcf075c7954
ipolevoy <igor@expresspigeon.com>
2019-08-30 15:02:11 -0500

    Reorganized to make environment-independent

--- content/src/main/resources/Common.groovy
+++ content/src/main/resources/Common.groovy
@@ -1,4 +1,4 @@
-package groovy
+
 
 import java.nio.file.Files
 import java.nio.file.Path
--- content/src/main/resources/ProcessMarkdown.groovy
+++ content/src/main/resources/ProcessMarkdown.groovy
@@ -1,40 +1,30 @@
-
-
 import java.nio.file.*
 
 GroovyShell shell = new GroovyShell()
-common = shell.parse(Paths.get("src/main/groovy/Common.groovy").toFile())
+common = shell.parse(Paths.get("${project.basedir}/target/classes/Common.groovy").toFile())
 
-String src = "src"
-String output = "target/output"
+String src = "${project.basedir}/src"
+String output = "${project.basedir}/target/output"
 String outBlog = "${output}/blog"
 String outImages = "${output}/content-images"
 String searchIndex = "${output}/search_index"
 
 new File(output).mkdir()
-new File("${output}/pages").mkdir()
 new File(outBlog).mkdir()
 new File(searchIndex).mkdir()
-
-
-println new File(outImages).mkdir()
-
-println new File(searchIndex).mkdir()
-println new File("${outBlog}/authors").mkdir()
-println new File("${outBlog}/posts").mkdir()
+new File(outImages).mkdir()
+new File(searchIndex).mkdir()
+new File("${outBlog}/authors").mkdir()
+new File("${outBlog}/posts").mkdir()
 
 common.copyFiles(Paths.get("${src}/content-images").toString(), Paths.get(outImages).toString())
 
-
 common.copyProperties("${src}/blog/authors", "${outBlog}/authors")
 common.copyProperties("${src}/blog/posts", "${outBlog}/posts")
-
-
 common.processMarkDown("${src}/blog/posts", "${outBlog}/posts")
 common.processMarkDown("${src}/blog/authors", "${outBlog}/authors")
 
 common.processMarkDown("${src}/activejdbc", "${output}/pages")
 common.processMarkDown("${src}/activeweb", "${output}/pages")
-
 common.copyFiles(Paths.get("${src}/activejdbc").toString(), "${output}/pages")
 common.copyFiles(Paths.get("${src}/activeweb").toString(), "${output}/pages")
